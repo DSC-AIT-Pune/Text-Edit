265ea70a1c139f559f2d94e6ccdde137e5d28670
MiliMade <milimadeemail@gmail.com>
2022-10-06 18:29:55 -0400

    Corrected the spelling of 'DarkMode' and 'LightMode' in the toggle label

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       document.getElementById('darkmode-id').style.color='white'
-      document.getElementById('darkmode-id').innerText = "Enable Lightmode"
+      document.getElementById('darkmode-id').innerText = "Enable LightMode"
       showAlert(" Dark mode has been enabled", "primary");
       document.title = 'TextEdit-Dark Mode';
 
@@ -38,7 +38,7 @@ function App() {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       document.getElementById('darkmode-id').style.color='#042743'
-      document.getElementById('darkmode-id').innerText = "Enable Darkmode"
+      document.getElementById('darkmode-id').innerText = "Enable DarkMode"
       showAlert("Light mode has been enabled", "primary");
       document.title = 'TextEdit-Light Mode';
 
