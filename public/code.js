3b57aa5b0ccf0fb502b2c45779a8655a6bf9409e
AdityaKURMI <94731548+AdityaKURMI@users.noreply.github.com>
2022-10-17 04:58:27 -0700

    chat room app
    Sir please fork this project as hactoberfest22

---
+++ public/code.js
@@ -0,0 +1,90 @@
+(function(){
+
+    const app = document.querySelector(".app");
+    const socket = io();
+
+    let uname;
+
+    app.querySelector(".join-screen #join-user").addEventListener("click",function(){
+        let username = app.querySelector(".join-screen #username").value;
+        if(username.length ==0){
+            return;
+        }
+        socket.emit("newuser",username);
+        uname = username;
+        app.querySelector(".join-screen").classList.remove("active");
+        app.querySelector(".chat-screen").classList.add("active");
+    });
+
+    app.querySelector(".chat-screen #send-message").addEventListener("click",function(){
+        const app = document.querySelector("#message_input");
+
+        let message = app.value;
+        console.log(message)
+
+       // let message = document.querySelector("#massage-input").value;
+        if(message.length == 0){
+            return null;
+        }
+        renderMessage("my",{
+            username:uname,
+            text:message
+        });
+        socket.emit("chat",{
+            username:uname, 
+            text:message
+        })
+        app.querySelector(".chat-screen #message-input").value = "";
+    });
+ 
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
+        socket.emit("exituser",uname);
+        window.location.href = window.location.href;
+    })
+
+    socket.on("update",function(update){
+        renderMessage("update",update);
+    });
+    socket.on("chat",function(message){
+        renderMessage("other",message);
+    });
+
+
+
+
+
+    function renderMessage(type,message){
+        let messageContainer = app.querySelector(".chat-screen .messages");
+        if(type == "my"){
+            let el = document.createElement("div");
+            el.setAttribute("class","message my-message");
+            el.innerHTML =`
+            <div>
+                <div class="name">You</div>
+                <div class="text0">${message.text}</div>
+            </div>
+        `;
+        messageContainer.appendChild(el);
+        } else if (type == "other"){
+            let el = document.createElement("div");
+            el.setAttribute("class","message other-message");
+            el.innerHTML = `
+            <div>
+                <div class="name">${message.username}</div>
+                <div class="text">${message.text}</div>               
+            </div>
+            
+            `;
+            messageContainer.appendChild(el);
+        } else if(type=="update"){
+            let el = document.createElement("div");
+            el.setAttribute("class","update");
+            el.innerText =   message;
+            messageContainer.appendChild(el);
+        }
+        //scroll down
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+
+    }
+
+})();
